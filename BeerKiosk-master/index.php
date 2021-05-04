df4565d36ffdf7e1bb61e3d2e9b66773cce25d1b
larmRyan <42777659+larmRyan@users.noreply.github.com>
2021-05-04 16:18:25 -0500

    Update index.php

--- BeerKiosk-master/index.php
+++ BeerKiosk-master/index.php
@@ -8,97 +8,94 @@ if(!$stmt){
 
 $stmt->execute();
 
-
 $stmt->bind_result($snackPrice,$status,$typeSnack);
-
-
 
 while($stmt->fetch()){
   echo "<script>console.log( 'Snack Objects: " . $snackPrice . "' );</script>";
 }
 $stmt->close();
 
-
-
-
-
 if($status == 0){
   echo "Kiosk is currently closed! Please contact a manager to bring it back up!";
   echo "<title>Currently Closed!</title></head>";
   exit();
 }
-
-echo "<div id=\"testDiv\">Currently Serving: {$typeSnack} </div>";
-echo "<div id =\"amountDiv\">Cost Per Snack: {$snackPrice}</div>";
-echo "<div id =\"flatDiv\"></div>";
-echo "<div id =\"taxDiv\"></div>";
-echo "<div id =\"totalCost\">{$snackPrice}</div>";
 ?>
 
 <html>
 <head>
-<!-- The Styling File -->
-<link rel="stylesheet" href="./style.css"/>
-<title>Snack Kiosk!</title>
-<script type = "text/javascript" src = "functional.js"> </script>
+  <!-- The Styling File -->
+  <link rel="stylesheet" href="./style.css"/>
+  <meta name="viewport" content="width=device-width,initial-scale=1.0">
+  <title>Snack Kiosk!</title>
+  <script type = "text/javascript" src = "functional.js"> </script>
 </head>
+
 <body>
-    <div id="paymentDiv">
-<form action="./charge.php" method="post" id="payment-form">
+  <form action="./charge.php" method="post" id="payment-form">
+    <h1>Beer Kiosk</h1>
 
-  <div id="inputDiv">
-    </br>
-    </br>
-        <center>
-            <h3>How many snacks would you like to purchase?</h1>
-                  <button id="minus" style="background-color: #008CBA; /* Green */
-                border: none;
-                color: white;
-                padding: 15px 32px;
-                text-align: center;
-                text-decoration: none;
-                display: inline-block;
-                font-size: 80px;border-radius: 12px;">-</button>      <input  type="number" name="amountOfSnacks" id="amountOfSnacks" style="font-size: 80px; width: 130px; color:#008CBA;"/>      <button id="plus" style="background-color: #008CBA; /* Green */
-border: none;
-color: white;
-padding: 15px 32px;
-text-align: center;
-text-decoration: none;
-display: inline-block;
-font-size: 80px;border-radius: 12px;">+</button>
-    </br>
-    </br>
-    </br>
-    <p>Enter Phone Number (Format: Area Code and Number): </p> 
-    <input type="tel" id="phone" name="phone" >
-  </br>
-</br>
+    <div class="center-items">
+      <div class="price_info">
+        <?php
+          echo "<div class=\"cost-div\" id=\"testDiv\">Currently Serving: {$typeSnack} </div>";
+          echo "<div class=\"cost-div\" id =\"amountDiv\">Cost Per Beer: {$snackPrice} </div>";
+          echo "<div class=\"cost-div\" id =\"flatDiv\"></div>";
+          echo "<div class=\"cost-div\" id =\"taxDiv\"></div>";
+          echo "<div class=\"cost-div\" id =\"totalCost\">{$snackPrice}</div>";
+        ?>
+      </div>
 
-            <button id="purchaseButton" style="background-color: #008CBA; /* Green */
-            border: none;
-            color: white;
-            padding: 15px 32px;
-            text-align: center;
-            text-decoration: none;
-            display: inline-block;
-            font-size: 50px;border-radius: 12px;">Next</button></center>
-        </div>  
-  <div class="form-row">
-    <label for="card-element">Credit or debit card</label>
-    <div id="card-element">
-      <!-- a Stripe Element will be inserted here. -->
+        <div id="paymentDiv">
+          <div id="inputDiv">
+            <div style="display: flex; flex-direction: column">
+              <input type="number" name="amountOfSnacks" id="amountOfSnacks"/>
+              <div style="display: flex; min-width: min-content">
+
+                <button id="minus">-</button>
+                <button id="plus">+</button>
+              </div>
+            </div>
+          </div>
+
+          <div id="credit-info">
+            <div class="form-row">
+              <label for="card-element">Credit or debit card</label>
+
+              <!-- a Stripe Element will be inserted here. -->
+              <div id="card-element"></div>
+
+              <!-- Used to display form errors -->
+              <div id="card-errors"></div>
+            </div>
+            <button>Submit Payment</button>
+          </div>
+
+        </div>
     </div>
-    <!-- Used to display form errors -->
-    <div id="card-errors"></div>
-  </div>
-  <button>Submit Payment</button> </div>
-</form>
-<!-- The needed JS files -->
-<!-- JQUERY File -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<!-- Stripe JS -->
-<script src="https://js.stripe.com/v3/"></script>
-<!-- Your JS File -->
-<script src="./charge.js"></script>
+
+    <label class="phone">
+      Phone Number <br />
+      <input class="phone" type="tel" id="phone" name="phone" />
+    </label>
+
+    <button id="purchaseButton">Next</button>
+
+
+    <div class="info" id="extraInfo">
+       <button onclick='window.location.href = "about.html"'>About</button>
+       <button onclick='window.location.href = "help.html"'>Help</button>
+       <button onclick='window.location.href = "faq.html"'>FAQ</button>
+   </div>
+  </form>
+
+  <!-- The needed JS files -->
+  <!-- JQUERY File -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <!-- Stripe JS -->
+  <script src="https://js.stripe.com/v3/"></script>
+  <!-- Your JS File -->
+  <script src="./charge.js"></script>
+
 </body>
 </html>
